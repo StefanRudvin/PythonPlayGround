9783dc42fec89394939dbbefbadb01ac77b30ce8
Stefan Rudvin <Stefan.rudvin@hotmail.com>
2017-01-24 17:12:14 +0000

    Improved, but outputs do not match 100%

--- algorithms/carry.py
+++ algorithms/carry.py
@@ -1,5 +1,18 @@
 #Algorithms practical 1
 
+'''
+
+6 operations
+299743898 716456069
+
+156188102
+193749048
+      150
+010011001
+
+
+'''
+
 while True:
 
     carrynumbers = 0
@@ -10,18 +23,31 @@ while True:
     digit1 = numbers[0]
     digit2 = numbers[1]
 
-    #print "First digit: " , digit1, "Second digit: ", digit2
-
     if digit1 == "0" and digit2 == "0":
         break
 
     carryNumbers = 0
 
-    for n in range(len(digit1)):
-        carry = int(digit1[n]) + int(digit2[n])
-        #print carry
+    n1 = len(digit1)-1
+    n2 = len(digit2)-1
+    carryOver = False
+
+    while n1 > -1 and n2 > -1:
+
+        carry = int(digit1[n1]) + int(digit2[n2])
+
+        if carryOver == True:
+            carry += 1
+            carryOver = False
+
         if carry >= 10:
             carryNumbers += 1
-        n += 1
+            carryOver= True
 
-    print "This has", carryNumbers , "Carries"
+        n1 -= 1
+        n2 -= 1
+
+    if carryNumbers == 0:
+        print "No carry operation."
+    else:
+        print carryNumbers , "carry operations."
